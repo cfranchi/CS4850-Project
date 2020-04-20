019b5e1f5737db9e36ada24e1cdf4f4a3c6b221e
cfranchi <cameron.franchi3@gmail.com>
2020-04-19 22:10:14 -0400

    Delete initialNode.py

--- initialNode.py
+++
@@ -1,8 +0,0 @@
-class initialNode:
-
-    def __init__(self, keywords, attributeType, name):
-        self.keywords = {}
-        self.attributeType = attributeType
-        self.name = name
-        for word in keywords:
-            self.keywords.update({word: keywords[word]})
